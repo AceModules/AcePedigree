d4a4b69e78d17b09ee8aa27b7440ca94a676df61
Claire Matthews <poeticdragon@stormblaze.net>
2019-08-23 14:20:40 -0700

    Added display of test mating pedigrees and analysis

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace AcePedigree\Controller;
 
+use AcePedigree\Entity;
 use Zend\Mvc\Controller\AbstractActionController;
 use Doctrine\ORM\EntityManager;
 
@@ -41,6 +42,33 @@ class IndexController extends AbstractActionController
     // Test mating pedigree
     public function testMatingAction()
     {
-        return [];
+        $repository = $this->entityManager->getRepository(Entity\Dog::class);
+
+        $entity = new Entity\Dog();
+        $entity->setName('Future Offspring');
+
+        $maxGen = (int) $this->params()->fromQuery('gens', 3);
+        $maxGen = min(9, max(3, $maxGen));
+
+        $sireId = (int) $this->params()->fromQuery('sire');
+        $sire = $repository->find($sireId);
+
+        if ($sire && $sire->getSex() == Entity\Dog::SEX_MALE) {
+            $entity->setSire($sire);
+            $repository->findByDescendant($sire, $maxGen - 1);
+        }
+
+        $damId = (int) $this->params()->fromQuery('dam');
+        $dam = $repository->find($damId);
+
+        if ($dam && $dam->getSex() == Entity\Dog::SEX_FEMALE) {
+            $entity->setDam($dam);
+            $repository->findByDescendant($dam, $maxGen - 1);
+        }
+
+        return [
+            'entity' => $entity,
+            'maxGen' => $maxGen,
+        ];
     }
 }

327e95eefb99083806f2272465ffbd1d13956979
Claire Matthews <poeticdragon@stormblaze.net>
2019-09-19 16:33:41 -0700

    Fixed SQL error when downgrading migration to rename kinship/ancestry

--- src/Migrations/Version20190919070547.php
+++ src/Migrations/Version20190919070547.php
@@ -33,7 +33,7 @@ final class Version20190919070547 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('RENAME TABLE pedigree_kinship TO pedigree_ancestry');
-        $this->addSql('ALTER TABLE pedigree_kinship RENAME INDEX IDX_52F7EC39D1F91D63 TO IDX_2058874D1F91D63');
-        $this->addSql('ALTER TABLE pedigree_kinship RENAME INDEX IDX_52F7EC39D3BFA33A TO IDX_2058874D3BFA33A');
+        $this->addSql('ALTER TABLE pedigree_ancestry RENAME INDEX IDX_52F7EC39D1F91D63 TO IDX_2058874D1F91D63');
+        $this->addSql('ALTER TABLE pedigree_ancestry RENAME INDEX IDX_52F7EC39D3BFA33A TO IDX_2058874D3BFA33A');
     }
 }

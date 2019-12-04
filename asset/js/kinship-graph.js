63651c5d60339082c48f7446e6f2ebee0450c4d3
Claire Matthews <poeticdragon@stormblaze.net>
2019-12-03 19:11:00 -0800

    Convert kinshipGraph function to jQuery method

--- asset/js/kinship-graph.js
+++ asset/js/kinship-graph.js
@@ -1,25 +1,27 @@
-function kinshipGraph($container, dataUrl) {
-    var Graph = ForceGraph3D()
-        ($container[0])
-        .width($container.width())
-        .height($container.width() * 0.6)
-        .backgroundColor('#ffffff')
-        .enableNodeDrag(false)
-        .nodeResolution(16)
-        .nodeColor(node => 'hsl(210, 65%, 50%)')
-        .nodeOpacity(1)
-        .nodeVal(node => node.value * 10)
-        .linkVisibility(false)
-        .jsonUrl(dataUrl);
+$(function() {
+    $.fn.kinshipGraph = function(dataUrl) {
+        var Graph = ForceGraph3D()
+            (this[0])
+            .width($(this).width())
+            .height($(this).width() * 0.6)
+            .backgroundColor('#ffffff')
+            .enableNodeDrag(false)
+            .nodeResolution(16)
+            .nodeColor(node => 'hsl(210, 65%, 50%)')
+            .nodeOpacity(1)
+            .nodeVal(node => node.value * 10)
+            .linkVisibility(false)
+            .jsonUrl(dataUrl);
 
-    Graph.d3Force('link').distance(link => link.distance * 100);
+        Graph.d3Force('link').distance(link => link.distance * 100);
 
-    let resizeGraph;
-    (resizeGraph = function() {
-        $(Graph.renderer().domElement)
-            .css('width', $container.width() + 'px')
-            .css('height', ($container.width() * 0.6) + 'px');
-    })();
+        let resizeGraph;
+        (resizeGraph = function() {
+            $(Graph.renderer().domElement)
+                .css('width', $(this).width() + 'px')
+                .css('height', ($(this).width() * 0.6) + 'px');
+        })();
 
-    $(window).on('resize', resizeGraph);
-}
+        $(window).on('resize', resizeGraph);
+    };
+});

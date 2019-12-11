e6f15f5e449f5dd3568254a61dc1a9a7e5079561
Claire Matthews <poeticdragon@stormblaze.net>
2019-12-10 21:30:44 -0800

    Renamed schema version table. Warning: WILL BREAK DB, start fresh

--- config/migrations.config.php
+++ config/migrations.config.php
@@ -6,5 +6,5 @@ return [
     'name'                 => 'AcePedigree Database Migrations',
     'migrations_directory' => __DIR__ . '/../src/Migrations',
     'migrations_namespace' => __NAMESPACE__ . '\Migrations',
-    'table_name'           => 'pedigree_schema',
+    'table_name'           => 'ace_schema',
 ];

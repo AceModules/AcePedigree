ffd8d11448ba383360fa7371eba43e2b3b15b4b8
Claire Matthews <poeticdragon@stormblaze.net>
2019-12-10 20:38:28 -0800

    Moved migrations config to separate file

---
+++ config/migrations.config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AcePedigree;
+
+return [
+    'name'                 => 'AcePedigree Database Migrations',
+    'migrations_directory' => __DIR__ . '/../src/Migrations',
+    'migrations_namespace' => __NAMESPACE__ . '\Migrations',
+    'table_name'           => 'pedigree_schema',
+];
--- config/module.config.php
+++ config/module.config.php
@@ -231,14 +231,6 @@ return [
                 ],
             ],
         ],
-        'migrations_configuration' => [
-            'orm_default' => [
-                'name'      => 'AcePedigree Migrations',
-                'directory' => __DIR__ . '/../src/Migrations',
-                'namespace' => __NAMESPACE__ . '\Migrations',
-                'table'     => 'pedigree_schema',
-            ],
-        ],
         'configuration' => [
             'orm_default' => [
                 'filter_schema_assets_expression' => '~^(?!pedigree_animal_statistics)~',

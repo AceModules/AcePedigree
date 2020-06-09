afa21e84d40c378d68e633f2d9e7f7afe90d5f82
Claire Matthews <poeticdragon@stormblaze.net>
2020-06-09 12:52:34 -0700

    Update pedigrees and kinship graph to use bootswatch styles

--- asset/js/ace-pedigree.js
+++ asset/js/ace-pedigree.js
@@ -4,16 +4,46 @@ $(function() {
             (this[0])
             .width($(this).width())
             .height($(this).width() * 0.6)
-            .backgroundColor('#ffffff')
+            .backgroundColor('rgba(0, 0, 0, 0)')
             .enableNodeDrag(false)
             .nodeResolution(16)
-            .nodeColor(node => 'hsl(210, 65%, 50%)')
+            .nodeColor(node => $('body').css('--primary'))
             .nodeOpacity(1)
             .nodeVal(node => node.value * 10)
             .linkVisibility(false)
             .jsonUrl(dataUrl);
 
         Graph.d3Force('link').distance(link => link.distance * 100);
+
+        var Observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.addedNodes.length == 1 && mutation.addedNodes[0].nodeType == 3) {
+                    $(mutation.target).html('<div class="tooltip-inner">' + $(mutation.target).text() + '</div>');
+                } else if (mutation.attributeName == 'href') {
+                    var oldColor = $('body').css('--primary');
+                    (function checkStyleUpdate(){
+                        var newColor = $('body').css('--primary');
+                        if (oldColor != newColor) {
+                            Graph.nodeColor(node => newColor);
+                        } else {
+                            setTimeout(checkStyleUpdate, 100);
+                        }
+                     })();
+                }
+            });
+        });
+
+        Observer.observe(
+            $('.scene-tooltip', this)
+                .removeClass('scene-tooltip')
+                .addClass('tooltip show')[0],
+            { childList: true }
+        );
+
+        Observer.observe(
+            $('link[title="theme"]')[0],
+            { attributes: true, attributeFilter: ['href'] }
+        );
 
         let resizeGraph;
         (resizeGraph = function() {
